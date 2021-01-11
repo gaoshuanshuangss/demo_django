3a2001f680ebbffb0374a6c4317cf0994cbf0cf2
gss <17051295524@163.com>
2021-01-11 22:00:47 +0800

    dele all

--- demo/__init__.py
+++
@@ -1,3 +0,0 @@
-import pymysql
-
-pymysql.install_as_MySQLdb()  # 配置mysql驱动
--- demo/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Students(models.Model):
-    name = models.CharField(max_length=16)
-    age = models.IntegerField(default=10)
-    sex = models.CharField(max_length=32)
